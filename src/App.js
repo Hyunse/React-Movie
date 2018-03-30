032c2ff4a40f2654b2f01db52fa0f7f01237b0fc
KimHyunse <anzmf12@naver.com>
2018-03-30 16:09:53 -0400

    css 제외

--- src/App.js
+++ src/App.js
@@ -3,34 +3,49 @@ import logo from './logo.svg';
 import './App.css';
 import Movie from './Movie';
 
-const movies = [
-  {
-    title: 'Matirx',
-    poster: 'https://lh3.googleusercontent.com/r1nBasWl5jpipLOx93lGbLH89vnCrhlRQAXr2co8XRFRW3INBh3kQpzPMAD2Conm0wsr=w200-h300'
-  },
-  {
-    title: 'Oldboy',
-    poster: 'https://i.ytimg.com/vi/2HkjrJ6IK5E/hqdefault.jpg'
-  },
-  {
-    title: 'Start Wars',
-    poster: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/Star_Wars_Logo.svg/1280px-Star_Wars_Logo.svg.png'
-  },
-  {
-    title: 'CoCo',
-    poster: 'https://d2e111jq13me73.cloudfront.net/sites/default/files/styles/product_image_aspect_switcher_170w/public/product-images/csm-movie/coco-poster0.jpg?itok=BrD_OWJI'
-  }
-];
-
 
 class App extends Component {
+
+  state = {
+    greeting: 'Hello',
+  }
+  
+  componentDidMount() {
+    this._getMovies();
+  }
+
+ _getMovies = async () => {
+    const movies = await this._callApi();
+    this.setState({
+      movies
+    })
+  }
+
+  _callApi = () => {
+    return fetch('https://yts.am/api/v2/list_movies.json?sort_by=rating')
+    .then(response => response.json())
+    .then(json => json.data.movies)
+    .catch(err => console.log(err))
+  }
+
+  _renderMovies = () => {
+    const movies = this.state.movies.map((movie) => {
+      return <Movie 
+              title={movie.title_english}
+              poster={movie.medium_cover_image} 
+              genres={movie.genres} key={movie.id}
+              synopsis={movie.synopsis}
+            />;
+    });
+    return movies;
+  }
+
   render() {
+    const { movie } = this.state;
+
     return (
-      // jsx
       <div className="App">
-        {movies.map((movie) => {
-          return <Movie title={movie.title} image={movie.poster}/>;
-        })}
+        {this.state.movies ? this._renderMovies() : 'Loading'}
       </div>
     );
   }
--- src/Movie.js
+++ src/Movie.js
@@ -1,24 +1,52 @@
 import React, { Component }  from 'react';
 import './Movie.css';
+import Prototypes from 'prop-types'
 
-class Movie extends Component {
-    render() {
-        return(
-            <div>
-                <MoviePost image={this.props.image}/>
-                <h1> It's {this.props.title} </h1>
+function Movie({title, poster, genres, synopsis}){
+    return (
+        <div className="Movie">
+            <div className="Movie_Columns">
+                <MoviePoster poster={poster} className="Movie_Poster" alt={title}/>
             </div>
-            
-        )
-    }
+            <div className="Movie_Columns">
+                <h1>{title}</h1>
+                <div className="Movie_Genres">
+                    {genres.map((genre, index ) => <MovieGenre genre={genre} key={index} />)}
+                </div>
+                <p className="Movie_Synopsis">
+                    {synopsis}
+                </p>
+            </div>
+        </div>
+    )
 }
 
-class MoviePost extends Component {
-    render() {
-        return (
-            <img src={this.props.image} alt=""/>
-        )
-    }
+function MoviePoster({poster, alt}){
+    return (
+        <img src={poster} title={alt} alt={alt}/>
+    )
+}
+
+function MovieGenre({genre}){
+    return (
+        <span className="Movie_Genre">{genre}</span>
+    )
+}
+
+Movie.propTypes = {
+    title: Prototypes.string.isRequired,
+    poster: Prototypes.string.isRequired,
+    genres: Prototypes.array.isRequired,
+    synopsis: Prototypes.string.isRequired
+}
+
+MoviePoster.prototypes = {
+    poster: Prototypes.string.isRequired,
+    alt: Prototypes.string.isRequired
+}
+
+MovieGenre.prototype = {
+    genre: Prototypes.string.isRequired
 }
 
 export default Movie
